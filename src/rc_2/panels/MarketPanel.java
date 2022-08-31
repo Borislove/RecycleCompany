c98eb8b39c2524d70745d97e6dbdd44360035368
Borislove <abirme@yandex.ru>
2022-08-31 14:49:19 +0300

    ......

--- src/rc_2/panels/MarketPanel.java
+++ src/rc_2/panels/MarketPanel.java
@@ -1,5 +1,7 @@
 package rc_2.panels;
 
+import rc_2.values.image_path.ImagePath;
+
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
@@ -9,8 +11,8 @@ public class MarketPanel extends JPanel {
     JLabel label;
 
     JPanel panel_1 = new JPanel();
-    JPanel panel_2 = new JPanel();
-    JPanel panel_3 = new JPanel();
+    JPanel panel_pizza = new JPanel();
+    JPanel panel_burger = new JPanel();
 
     public MarketPanel() {
 
@@ -18,20 +20,47 @@ public class MarketPanel extends JPanel {
         add(label);
 
         Border solidBorder = BorderFactory.createLineBorder(Color.green, 2);
+        Border orangeBorder = BorderFactory.createLineBorder(Color.orange, 3);
 
         setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 
 
+        ////////////////////////////Бутылка//////////////////////////////////////////
         add(panel_1);
         panel_1.setBackground(Color.black);
         panel_1.setBorder(solidBorder);
 
-        add(panel_2);
-        panel_2.setBackground(Color.black);
-        panel_2.setBorder(solidBorder);
-        add(panel_3);
-        panel_3.setBackground(Color.black);
-        panel_3.setBorder(solidBorder);
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.gray);
+
+        JLabel label = new JLabel();
+        panel.add(label);
+        label.setIcon(ImagePath.imageBottle);
+        panel.setBorder(orangeBorder);
+        panel_1.add(panel);
+
+        /////////////////////////////ПИЦЦА//////////////////////////////////////////
+        add(panel_pizza);
+        panel_pizza.setBackground(Color.white);
+        panel_pizza.setBorder(solidBorder);
+    //    panel_pizza.setBackground(Color.gray);
+
+        JLabel labelPizza = new JLabel();
+        panel_pizza.add(labelPizza);
+        labelPizza.setIcon(ImagePath.imagePizza);
+        panel_pizza.setBorder(orangeBorder);
+        panel_pizza.add(labelPizza);
+
+        ////////////////////////////БУРГЕР//////////////////////////////////////////
+        add(panel_burger);
+        panel_burger.setBackground(Color.white);
+        panel_burger.setBorder(solidBorder);
+        JPanel panel2 = new JPanel();
+        panel2.setBackground(Color.gray);
+        JLabel label2 = new JLabel();
+        panel2.setBorder(orangeBorder);
+        label2.setIcon(ImagePath.imageBurger);
+        panel_burger.add(label2);
 
 
         setBorder(BorderFactory.createEtchedBorder());
--- src/rc_2/values/image_path/ImagePath.java
+++ src/rc_2/values/image_path/ImagePath.java
@@ -4,6 +4,9 @@ import javax.swing.*;
 
 public class ImagePath {
 
+
+    //ICON TABBED
+
     public static ImageIcon iconShop = new ImageIcon("E:\\ПРОГРАММИРОВАНИЕ\\RecycleCompany\\shopImage\\shop.png");
 
     public static ImageIcon iconSearch = new ImageIcon("E:\\ПРОГРАММИРОВАНИЕ\\RecycleCompany\\shopImage\\scrap.png");
@@ -12,4 +15,9 @@ public class ImagePath {
 
     public static ImageIcon iconMarket = new ImageIcon("E:\\ПРОГРАММИРОВАНИЕ\\RecycleCompany\\shopImage\\healthy-food.png");
 
+
+    //Image market
+    public static ImageIcon imageBottle = new ImageIcon("E:\\ПРОГРАММИРОВАНИЕ\\RecycleCompany\\marketImage\\plastic-bottle.png");
+    public static ImageIcon imagePizza = new ImageIcon("E:\\ПРОГРАММИРОВАНИЕ\\RecycleCompany\\marketImage\\pizza.png");
+    public static ImageIcon imageBurger = new ImageIcon("E:\\ПРОГРАММИРОВАНИЕ\\RecycleCompany\\marketImage\\burger.png");
 }
